99f7ee6d394e8431c11eeea43d94e7fa2eeb9586
vuongvjppro12e <notaku202@gmail.com>
2022-08-24 11:42:58 +0700

    full_java6

--- j6store/src/main/java/com/example/j6store/dao/OrderDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.j6store.dao;
-
-import com.example.j6store.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDAO extends JpaRepository<Order, Long> {
-}
--- j6store/src/main/java/com/example/j6store/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.example.j6store.service;
-
-import com.example.j6store.entity.Product;
-
-import java.util.List;
-
-public interface ProductService {
-    List<Product> findAll();
-}
